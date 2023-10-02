6450feffae11b3faa9f85c7884fa76a813cdb1fd
SanjeevJogur <132989815+SanjeevJogur@users.noreply.github.com>
2023-10-02 12:17:35 +0000

    variable discuss

---
+++ 02_variable.js
@@ -0,0 +1,15 @@
+const rollno=123456
+let email="sanjeev@gmail.com"
+var password="120120"
+city="Vijayapur"
+
+// rollno=120230
+
+// console.log(rollno);
+
+
+email="sanj@gmail.com"
+password="46565"
+city="Mysore"
+
+console.table([rollno,email,password,city])
