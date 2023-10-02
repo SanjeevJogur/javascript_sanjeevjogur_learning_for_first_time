fe1db588e9cd02d94d0698bf1924f1bffcfcbec4
SanjeevJogur <132989815+SanjeevJogur@users.noreply.github.com>
2023-10-02 12:19:29 +0000

    datatype done

---
+++ 03_datatype.js
@@ -0,0 +1,17 @@
+let name="Sanjeev"
+let age=21
+// datatypes
+//number
+// bigint
+// boolean
+// string
+// null stand alone value
+// undefined
+// symbol => unique
+
+// object
+console.log(typeof "Sanjeev"); 
+console.log(typeof age); 
+console.log(typeof undefined);//op=undefined
+console.log(typeof null);//op=object
+
