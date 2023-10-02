15ad4f695ec4269030986714953371152400b38b
SanjeevJogur <132989815+SanjeevJogur@users.noreply.github.com>
2023-10-02 13:41:18 +0000

    test done

---
+++ conversionop.js
@@ -0,0 +1,38 @@
+let score="100aa"
+
+console.log(typeof score);
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+let loggedin=1
+let b=Boolean(loggedin)
+console.log(b);
+let s=100
+let ss=String(s)
+console.log(s);
+console.log(typeof ss);
+
+
+// operations
+let v=10
+let n=-v
+console.log(n);
+console.log(25**3);
+
+let s1="sanjeev"
+let s2="jogur"
+
+s3=s1+s2
+console.log(s3);
+console.log("1"+2);
+console.log(1+"2");
+console.log(1+3+"2");
+console.log("1"+3+2);
+console.log(true);
+console.log(+true);
+// console.log(true+);
+// console.log(""+);
+console.log(+"");
+
+
