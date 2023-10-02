9f8f4b8113c67b4c1637c289b208111a11346192
SanjeevJogur <132989815+SanjeevJogur@users.noreply.github.com>
2023-10-02 14:42:18 +0000

    comparision done

---
+++ 04_comparision.js
@@ -0,0 +1,16 @@
+console.log("5"==5);
+console.log(null == 0);
+console.log(null >0);
+console.log(null <0);
+console.log(null != 0);
+
+console.log(undefined==0);
+console.log(undefined<=0);
+console.log(undefined>=0);
+console.log(undefined!=0);
+
+// This is conditions are lead to confussion
+
+// === condition check without changing datatype
+
+console.log("5"===5);
