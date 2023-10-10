274051152fbaf7986107892ad692731c9fe9df6c
SanjeevJogur <132989815+SanjeevJogur@users.noreply.github.com>
2023-10-10 15:07:08 +0000

    done

---
+++ 05_string.js
@@ -0,0 +1,7 @@
+const name="Sanjeev"
+const rollno=70
+
+console.log(name+ rollno+ " ASPBCA");
+console.log(` My name is ${name} and rollno is ${rollno}`);
+const myname= new String('Sanjeev Jogur')
+console.log(myname[0]);
