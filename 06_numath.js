53253d04b926e1740373cfebf6b58e1a1297f58b
SanjeevJogur <132989815+SanjeevJogur@users.noreply.github.com>
2023-12-17 11:46:24 +0000

    num and math

---
+++ 06_numath.js
@@ -0,0 +1,38 @@
+const marks=89
+console.log(marks);
+
+const fees=new Number(24000)
+console.log(fees);
+console.log(fees.toString());
+
+console.log(fees.toFixed(2));
+
+const result=89.6524675
+console.log(result.toPrecision(3));
+
+
+
+const pg=100000
+console.log(pg.toLocaleString('en-US'));
+console.log(pg.toLocaleString('en-IN'));
+
+
+//=========MY Jaan maths=========
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.5));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
+
+
+console.log(Math.random());
+console.log(Math.random()*100)+10;
+console.log(Math.floor(Math.random()*100)+10);
+
+const min=100
+const max=200
+
+console.log(Math.floor(Math.random()*(max-min+10))+min);
+
+
+
