39414e8f52d845ac5c2df18e9f468f11c28c25f3
SanjeevJogur <132989815+SanjeevJogur@users.noreply.github.com>
2023-12-19 07:21:53 +0000

    fun

---
+++ part-3/01_functions.js
@@ -0,0 +1,12 @@
+
+
+function myname(){
+    console.log("S");
+    console.log("A");
+    console.log("N");
+    console.log("J");
+    console.log("E");
+    console.log("E");
+    console.log("V");
+}
+myname()
