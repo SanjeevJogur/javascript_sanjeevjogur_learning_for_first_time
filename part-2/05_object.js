b9a38220d4df5ae1367dd17957afe914df472e05
SanjeevJogur <132989815+SanjeevJogur@users.noreply.github.com>
2023-12-19 07:09:05 +0000

    obj

---
+++ part-2/05_object.js
@@ -0,0 +1,15 @@
+const course={
+    coursename:"Javascript",
+    fee:999,
+    Instructor:"Sanjeev"
+}
+// course.Instructor
+
+const {Instructor}=course
+console.log(Instructor);
+
+// {
+//     "name":"Siddu",
+//     "Coursename":"CSS",
+//     "Price":1999
+// }
