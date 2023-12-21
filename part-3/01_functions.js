18c67c9087a04845492f54c4ebbe39ed99c776c6
SanjeevJogur <132989815+SanjeevJogur@users.noreply.github.com>
2023-12-21 15:29:24 +0000

    fun

--- part-3/01_functions.js
+++ part-3/01_functions.js
@@ -10,3 +10,24 @@ function myname(){
     console.log("V");
 }
 myname()
+function addtwono(n1,n2){
+    console.log(n1+n2);
+    // let ans=n1+n2
+    // return ans
+    return n1+n2
+
+}
+
+addtwono(20,55)
+addtwono(20,"55")
+addtwono(20,null)
+
+const ans=addtwono(15,25)
+console.log("Answer:",ans);
+
+function developer(Username){
+    if(Username===undefined)
+    return `${Username} you are master`
+}
+console.log(developer("Siddu Mashyal"));
+
